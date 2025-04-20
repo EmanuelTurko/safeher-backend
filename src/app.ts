b63a43d6170c63f3a2eff5673b45d420ee9ff584
Ilana1505 <bilana4@cs.colman.ac.il>
2025-04-20 18:14:55 +0300

    Authentication

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 
 const app = express();
 
+app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
+
+app.use('/api/auth', authRoutes);
+
+
 app.get('/test', (req,res) => {
     res.json({ message: 'Test123'});
 });
-
-
-
-
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-import app from './app';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); 
+
+import app from './app';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 3000;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log(' MongoDB connected');
+    } catch (error) {
+        console.error(' MongoDB connection error:', error);
+        process.exit(1); 
+    }
+};
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
 
