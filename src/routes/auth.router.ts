04038c3c806ed0f704a6275aec77df771d93776f
Ilana1505 <bilana4@cs.colman.ac.il>
2025-05-28 13:34:29 +0300

    Forgot Password

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { login, loginWithGoogle, register } from "../controllers/auth.controller";
 import { getUserProfile ,updateUserSafeCircle} from "../controllers/user.controller";
 import { AuthenticatedRequest, authMiddleware } from "../middleware/auth.middleware";
+import { Request, Response } from "express";
 
 dotenv.config();
 
@@ -17,3 +18,14 @@ authRouter.post("/login-with-google", loginWithGoogle);
 authRouter.post("/updateUserSafeCircle",authMiddleware ,updateUserSafeCircle);
 
 authRouter.get("/check", authMiddleware, (req, res) => getUserProfile(req as AuthenticatedRequest, res));
+
+authRouter.post("/forgot-password", async (req: Request, res: Response): Promise<void> => {
+  const { email } = req.body;
+
+  if (!email) {
+    res.status(400).json({ message: "Email is required" });
+    return;
+  }
+
+  res.status(200).json({ message: "Password reset link sent successfully" });
+});
